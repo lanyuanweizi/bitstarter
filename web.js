d4849a5cb998b5d925fe4f806da46cd37a2650a6
lanyuanweizi <lanyuanweizi@126.com>
2013-07-10 23:14:33 +0800

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('fs.writeFile('index.html', 'Hello Node', function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
+});');
 });
 
 var port = process.env.PORT || 5000;
